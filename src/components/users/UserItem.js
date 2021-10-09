8beba19bd200b7c5d3050019912d18efe2ffa243
Zigmal <rafal.hes@yahoo.com>
2021-10-09 10:46:43 +0200

    Refactor UserItem component

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,22 +1,20 @@
-import { faTheRedYeti } from '@fortawesome/free-brands-svg-icons';
 import React, { Component } from 'react'
 
 class UserItem extends Component {
-    constructor() {
-        super();
-        this.state = {
-            id: 'id',
-            login: 'mojombo',
-            avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-            html_url: 'https://github.com/mojombo' 
-        }
-    }
+    state = {
+        id: 'id',
+        login: 'mojombo',
+        avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
+        html_url: 'https://github.com/mojombo' 
+    };
+
     render() {
+        const { avatar_url, login, html_url } = this.state;
         return (
             <div className="card text-center">
-                <img src={this.state.avatar_url} alt="" className="round-img" style={{ width: "60px" }}/>
-                <h3>{ this.state.login }</h3>
-                <a href={this.state.html_url} className="btn btn-dark btn-sn my-1" >More</a>
+                <img src={ avatar_url } alt="" className="round-img" style={{ width: "60px" }}/>
+                <h3>{ login }</h3>
+                <a href={ html_url } className="btn btn-dark btn-sn my-1" >More</a>
             </div>
         )
     }
