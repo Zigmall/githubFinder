4ea050ce1ab7f1da3c825343a5ceb44801cc9aff
Zigmal <rafal.hes@yahoo.com>
2021-10-09 10:27:08 +0200

    Add UserItem component

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import PropTypes from 'prop-types'
+
 
 
 
 export class Navbar extends Component {
+    static defaultProps = {
+        title: 'Github Finder',
+        icon: faGithub
+    }
+
+    static propTypes = {
+        title: PropTypes.string.isRequired,
+        // icon: PropTypes.icon.isRequired
+    }
+
     render() {
         return (
             <nav className='navbar bg-primary'>
                 <h1>
-                    <FontAwesomeIcon icon={faGithub} />
-                    Navbar
+                    <FontAwesomeIcon icon={this.props.icon} /> {this.props.title} 
                 </h1>
           </nav>
         )
