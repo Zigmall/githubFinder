506779b000c49bc0abf0250320b9f25aabbef641
Zigmal <rafal.hes@yahoo.com>
2021-10-10 17:56:16 +0200

    Add authentication - github_id and github_secret

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,22 +1,39 @@
-import React, { Component } from 'react'
+import React from 'react'
 import UserItem from './UserItem'
+import Spinner from '../layout/Spinner'
+import PropTypes from 'prop-types'
 
-class Users extends Component {
+
+const Users = ({ users, loading }) => {
     
-    render() {
-        return (
-            <div style={userStyle}>
-                {this.props.users.map(user => (
-                    <UserItem key={user.id} user={user}    />
+        
+      
+        
+        if(loading) {
+           return <Spinner />
+        } else {
+            return (
+            <div style={userStyle}>    
+                {users.map(user => (
+                    <UserItem key={user.id} user={user} />
                 ))}
-            </div>
-        )
-    }
+            </div> 
+            )
+        }
+
+         
+    
 }
 const userStyle = {
     display: 'grid',
     gridTemplateColumns: 'repeat(3, 1fr)',
     gridGap: '1rem'
 };
+
+Users.propTypes = {
+    users: PropTypes.array.isRequired,
+    loading: PropTypes.bool.isRequired,
+
+}
 
 export default Users
